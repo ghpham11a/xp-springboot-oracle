30affca5b40d11ed284c52279a7600ba272c51c0
Anthony Pham <user@Anthonys-MacBook-Pro.local>
2025-02-27 15:44:43 -0500

    init

---
+++ src/main/java/com/gpham11a/xp_springboot_oracle/XpSpringbootOracleApplication.java
@@ -0,0 +1,13 @@
+package com.gpham11a.xp_springboot_oracle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XpSpringbootOracleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XpSpringbootOracleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gpham11a/xp_springboot_oracle/XpSpringbootOracleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gpham11a.xp_springboot_oracle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XpSpringbootOracleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
